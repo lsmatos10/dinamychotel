bf4314072e4affa53a1302844f7b18c91b283e36
Leonardo Matos <devleomatos@gmail.com>
2022-09-22 14:21:27 -0300

    implementa react

---
+++ dinamyc/src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title">Bem Vindo!</span>
+            <span className="login-form-title">HotelDinamyc</span>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
