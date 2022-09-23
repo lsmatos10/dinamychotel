eb668b50d7bb4c42719868533d5d6abd8e38050a
Leonardo Matos <devleomatos@gmail.com>
2022-09-22 21:54:42 -0300

    Criação da tela de login já no react

--- dinamyc/src/App.js
+++ dinamyc/src/App.js
@@ -1,4 +1,5 @@
 
+import './styles.css'
 
 function App() {
   return (
@@ -7,7 +8,27 @@ function App() {
         <div className="wrap-login">
           <form className="login-form">
             <span className="login-form-title">Bem Vindo!</span>
-            <span className="login-form-title">HotelDinamyc</span>
+            <span className="login-form-title">Hotel Dinamyc</span>
+
+            <div className="wrap-input">
+              <input className="input" type="email" />
+                <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input className="input" type="password" />
+                <span className="focus-input" data-placeholder="Password"></span>
+
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Entrar</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Não possui conta?</span>
+              <a className="txt2" href="#">Criar conta.</a>
+            </div>
           </form>
         </div>
       </div>
