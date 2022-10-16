784842cd723878e587b78af977967bbebb363ee4
Leonardo Matos <devleomatos@gmail.com>
2022-10-16 15:52:20 -0300

    Prototipo v4
    Foi adicionado Harmonia de cores nas telas restantes, adicionado o campo CPF em cadastro, e adicionado mascara nos campos em CPF e Telefone, ajuste de bordas da tela reservas de quarto e cadastro

---
+++ usuario/cadastro.js
@@ -0,0 +1,36 @@
+
+
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,""); 
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2");
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2");
+    return v;
+}
+function id( el ){
+	return document.getElementById( el );
+}
+window.onload = function(){
+	id('telefone').onkeyup = function(){
+		mascara( this, mtel );
+	}
+
+    const inputCpf = document.querySelector("#cpf")
+
+
+inputCpf.addEventListener('keypress', function(){
+    let inputLength = inputCpf.value.length
+    if(inputLength === 3 || inputLength === 7){
+        inputCpf.value += '.'
+    }else if(inputLength === 11){
+        inputCpf.value += '-'
+    }
+})
+}
